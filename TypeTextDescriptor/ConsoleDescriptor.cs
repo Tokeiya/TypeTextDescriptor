893c461341cc168352116aabe1504b785eebbb58
Tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2020-04-12 16:02:13 +0900

    modify interface.

--- TypeTextDescriptor/ConsoleDescriptor.cs
+++ TypeTextDescriptor/ConsoleDescriptor.cs
@@ -1,47 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq.Expressions;
 
 namespace Tokeiya3.TypeTextDescriptor
 {
 	public static class ConsoleDescriptor
 	{
-		public static void Describe<T>(this T value,string nullSurrogation="NULL")
+		public static void Describe<T>(this T value,string nullSurrogation="NULL",int indent=2)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
 		}
 
-		public static void Describe<T>(this T value,string nullSurrogation="NULL",
+		public static void Describe<T>(this T value,string nullSurrogation="NULL",int indent=2,
 			params Expression<Func<T,string>>[] presenter)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
 		}
 
-		public static void Describe<T>(this T value,string nullSurrogateion="NULL",
+		public static void Describe<T>(this T value,string nullSurrogateion="NULL",int indent=2,
 			params (Func<T,string> selector,string title)[] presenter)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
 		}
 
-		public static void Describe<T>(this IEnumerable<T> value, string nullSurrogation = "NULL",bool pause=false)
+		public static void Describe<T>(this IEnumerable<T> value, string nullSurrogation = "NULL",int indent=2
+			,bool pause=false)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
 		}
 
 		public static void Describe<T>(this IEnumerable<T> value, string nullSurrogation = "NULL", bool pause=false,
-			params Expression<Func<T,string>>[] presenter)
+			int indent=2,params Expression<Func<T,string>>[] presenter)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
 		}
 
 		public static void Describe<T>(this IEnumerable<T> value,string nullSurrogation="NULL",bool pause=false,
-			params (Func<T,string> selector,string title)[] presenter)
+			int indent=2,params (Func<T,string> selector,string title)[] presenter)
 		{
 #warning NotImplemented
 			throw new NotImplementedException();
