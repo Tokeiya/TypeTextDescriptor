b9c8321e7ba34a29c7e10dff29eb5e169c3ba773
Tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2020-04-13 18:19:56 +0900

    Implement ImaginaryRoot class. Test passed, but not coverage check.

--- RecordFormatter/RecordLabel.cs
+++ RecordFormatter/RecordLabel.cs
@@ -16,24 +16,18 @@ namespace RecordFormatter
 	internal class ImaginaryRoot : IRecordLabel
 	{
 		public static IRecordLabel Root { get; } = new ImaginaryRoot();
-		public IRecordLabel Parent => throw new NotImplementedException();
+		public IRecordLabel Parent => throw new NotSupportedException($"{nameof(Parent)} is not supported.");
 
 
 		private ImaginaryRoot() { }
 
 		public void Add(string fieldId)
-		{
-			throw new NotImplementedException();
-		}
+			=> throw new NotSupportedException($"{nameof(Add)} is not supported.");
 
 		public IRecordLabel Add()
-		{
-			throw new NotImplementedException();
-		}
+			=> throw new NotSupportedException($"{nameof(Add)} is not supported.");
 
 		public void ToRecord(StringBuilder buffer)
-		{
-			throw new NotImplementedException();
-		}
+			=> throw new NotSupportedException($"{nameof(ToRecord)} is not supported.");
 	}
 }
