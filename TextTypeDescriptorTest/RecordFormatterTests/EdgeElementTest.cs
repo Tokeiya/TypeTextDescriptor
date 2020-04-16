c2b93cb08f613c96521b88c2806198b8b54def24
Tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2020-04-16 13:28:08 +0900

    Implement EdgeElementTest test passed. But Coverage was unchecked.
    Add ParsecSharp lib.

--- TextTypeDescriptorTest/RecordFormatterTests/EdgeElementTest.cs
+++ TextTypeDescriptorTest/RecordFormatterTests/EdgeElementTest.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using System.Text;
+using ChainingAssertion;
+using RecordFormatter;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,6 +13,34 @@ namespace TextTypeDescriptorTest.RecordFormatterTests
 
 		public EdgeElementTest(ITestOutputHelper output) => _output = output;
 
+		static void Check(EdgeElement actual, string expected)
+		{
+			var act = actual as IDescribable;
+			act.IsNotNull();
+			
+			var bld=new StringBuilder();
+			act.Describe(bld);
+			bld.ToString().Is(expected+"\r\n");
+			bld.Clear();
+			
+			using var wtr=new StringWriter(bld);
+			act.Describe(wtr);
+			bld.ToString().Is(expected+"\r\n");
+		}
+		
+		
+		[Fact]
+		public void InitTest()
+		{
+			var actual = new EdgeElement(42, 114514, "hoge");
+			actual.From.Is(42);
+			actual.To.Is(114514);
+			actual.Label.Is("hoge");
+			
+			Check(actual,"42 -> 114514 [label=\"hoge\"];");
+		}
+		
+		
 
 	}
 }
